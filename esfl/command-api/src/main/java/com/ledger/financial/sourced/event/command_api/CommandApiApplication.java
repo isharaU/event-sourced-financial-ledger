cfdb98d93d118ca7193d611d96232b571e6cd7be
isharaU <isharauditha5@gmail.com>
2026-05-03 23:14:08 +0530

    feat(#10): initialize multi-module project structure with command-api module and Maven wrapper

---
+++ esfl/command-api/src/main/java/com/ledger/financial/sourced/event/command_api/CommandApiApplication.java
@@ -0,0 +1,13 @@
+package com.ledger.financial.sourced.event.command_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandApiApplication.class, args);
+	}
+
+}
