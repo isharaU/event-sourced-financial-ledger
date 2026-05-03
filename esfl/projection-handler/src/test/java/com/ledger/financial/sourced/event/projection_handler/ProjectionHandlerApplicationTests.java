9554ab28e22fbb3fd1a9d14b6799e70b6e031d68
isharaU <isharauditha5@gmail.com>
2026-05-03 23:53:40 +0530

    feat(#10): add projection-handler module and update parent pom configuration

---
+++ esfl/projection-handler/src/test/java/com/ledger/financial/sourced/event/projection_handler/ProjectionHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledger.financial.sourced.event.projection_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectionHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
