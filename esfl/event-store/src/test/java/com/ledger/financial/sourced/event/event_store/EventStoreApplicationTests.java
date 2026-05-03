20ae8c9042f83b85417df830e47725f166564beb
isharaU <isharauditha5@gmail.com>
2026-05-03 23:49:14 +0530

    feat(#10): initialize event-store microservice with Maven structure and Docker support

---
+++ esfl/event-store/src/test/java/com/ledger/financial/sourced/event/event_store/EventStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledger.financial.sourced.event.event_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
