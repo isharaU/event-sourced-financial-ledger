124e0816444ecf8d13f11ffc0cbc950fbeb35780
isharaU <isharauditha5@gmail.com>
2026-05-21 00:00:11 +0530

    refactor(#13): reorder import statements in FlywayConfig for consistency

--- esfl/event-store/src/main/java/com/ledger/financial/sourced/event/event_store/config/FlywayConfig.java
+++ esfl/event-store/src/main/java/com/ledger/financial/sourced/event/event_store/config/FlywayConfig.java
@@ -1,10 +1,10 @@
 package com.ledger.financial.sourced.event.event_store.config;
 
+import javax.sql.DataSource;
+
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class FlywayConfig {
