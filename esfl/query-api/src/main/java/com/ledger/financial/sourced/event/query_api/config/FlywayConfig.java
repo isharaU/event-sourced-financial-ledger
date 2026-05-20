0b699a01cccdcbd9bbcb55c32a0650d642709b67
isharaU <isharauditha5@gmail.com>
2026-05-20 23:59:27 +0530

    feat(#13): add Flyway configuration to automatically run database migrations on startup

---
+++ esfl/query-api/src/main/java/com/ledger/financial/sourced/event/query_api/config/FlywayConfig.java
@@ -0,0 +1,21 @@
+package com.ledger.financial.sourced.event.query_api.config;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway(DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(false)
+                .validateOnMigrate(true)
+                .load();
+    }
+}
